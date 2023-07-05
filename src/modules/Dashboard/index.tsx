b1e89d875d64ee38ed1878c27469ce10f9dea4c4
Tiger Darbey <tiger@pop-os.localdomain>
2023-07-05 07:34:58 +0530

    remove status group cpmponent

--- src/modules/Dashboard/index.tsx
+++ src/modules/Dashboard/index.tsx
@@ -1,13 +1,9 @@
-'use client';
+// 'use client'
 
-import { Box, Flex, Grid } from '@mantine/core';
-import { StatsGroup } from '@/components/StatsGroup';
-import { mockData } from '@/components/StatsGroup/mock';
-import { BalanceCard } from './BalanceCard';
-// import { OverviewCard } from './OverviewCard';
-import { ProfileCard } from './ProfileCard';
-import { WelcomeCard } from './WelcomeCard';
-import StatsGrid from './StatsGrid';
+import { Box, Grid } from '@mantine/core'
+import { BalanceCard } from './BalanceCard'
+import { ProfileCard } from './ProfileCard'
+import StatsGrid from './StatsGrid'
 
 export function Dashboard() {
   return (
@@ -19,17 +15,8 @@ export function Dashboard() {
 		  <ProfileCard />
 		</Grid.Col>
 		<Grid.Col span={8}>
-		  <Flex direction="column" h="100%" justify="space-between">
-			<WelcomeCard />
-			<StatsGroup data={mockData} />
-		  </Flex>
-		</Grid.Col>
-		<Grid.Col span={8}>
 		  <BalanceCard />
 		</Grid.Col>
-		{/* <Grid.Col span={4}>
-			<OverviewCard />
-		</Grid.Col> */}
 	  </Grid>
 	</Box>
   )
