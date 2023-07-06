1ca589646f939398ddd59741bfc9372da7406fd7
Tiger Darbey <tiger@pop-os.localdomain>
2023-07-06 13:55:10 +0530

    create separate feature grid component

---
+++ src/sections/features/FeaturesGrid/FeatureGridCard.tsx
@@ -0,0 +1,24 @@
+import { gradientRadial } from "@/styles/constants"
+import { FeatureProps } from "@/types/types"
+import { Text, ThemeIcon, useMantineTheme } from "@mantine/core"
+
+export function FeatureGridCard({ icon: Icon, title, description }: FeatureProps) {
+  const theme = useMantineTheme()
+  return (
+    <div>
+      <ThemeIcon
+        size={50} radius={50}
+        variant="gradient"
+        gradient={gradientRadial}
+      >
+        <Icon size={30} stroke={2} />
+      </ThemeIcon>
+      <Text style={{ marginTop: theme.spacing.sm, marginBottom: 7 }}>
+        {title}
+      </Text>
+      <Text size="sm" color="dimmed" style={{ lineHeight: 1.6 }}>
+        {description}
+      </Text>
+    </div>
+  )
+}
