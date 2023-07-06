ef50d57749ea91bef9fe54b7a32a59727582fd4e
Tiger Darbey <tiger@pop-os.localdomain>
2023-07-06 17:18:48 +0530

    make her owrapper background white

--- src/components/wrappers/HeroWrapper/styles.ts
+++ src/components/wrappers/HeroWrapper/styles.ts
@@ -4,6 +4,6 @@ export const heroWrapperStyles = createStyles((theme) => ({
   wrapper: {
     position: 'relative',
     boxSizing: 'border-box',
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0], // theme.white
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white
   },
 }))
