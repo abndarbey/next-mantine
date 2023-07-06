187f4ea8ae22262e93e86ac75028ed11cf027cf8
Tiger Darbey <tiger@pop-os.localdomain>
2023-07-06 13:54:27 +0530

    add gradient constants

---
+++ src/styles/constants.ts
@@ -0,0 +1,16 @@
+type Gradient = {
+    deg?: number
+    from: string
+    to: string
+}
+
+export const gradientLiner: Gradient = {
+    from: 'teal',
+    to: 'cyan',
+}
+
+export const gradientRadial: Gradient = {
+    deg: 133,
+    from: 'teal',
+    to: 'cyan',
+}
