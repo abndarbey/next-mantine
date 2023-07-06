5744ba02044cb6de2a9c94855d453e61df58c734
Tiger Darbey <tiger@pop-os.localdomain>
2023-07-06 19:57:32 +0530

    increase card size on hover

--- src/sections/features/FeatureCards/styles.ts
+++ src/sections/features/FeatureCards/styles.ts
@@ -1,26 +1,32 @@
 import { createStyles } from "@mantine/core"
 
 export const featureCardsStyles = createStyles((theme) => ({
-    card: {
-        cursor: 'pointer',
-        border: `1px solid ${
-            theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
-        }`,
-    },
-    
-    link: {
-        textDecoration: 'none',
-        textDecorationColor: 'none',
-    },
+  card: {
+    cursor: 'pointer',
+    transition: 'transform 150ms ease, box-shadow 150ms ease',
+    border: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1]
+    }`,
 
-    cardTitle: {
-        '&::after': {
-            content: '""',
-            display: 'block',
-            backgroundColor: theme.fn.primaryColor(),
-            width: 45,
-            height: 2,
-            marginTop: theme.spacing.sm,
-        },
+    '&:hover': {
+      transform: 'scale(1.02)',
+      boxShadow: theme.shadows.md,
     },
+  },
+    
+  link: {
+    textDecoration: 'none',
+    textDecorationColor: 'none',
+  },
+
+  cardTitle: {
+    '&::after': {
+        content: '""',
+        display: 'block',
+        backgroundColor: theme.fn.primaryColor(),
+        width: 45,
+        height: 2,
+        marginTop: theme.spacing.sm,
+    },
+  },
 }))
