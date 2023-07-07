8c7ec1ffedcb98de34b568aaef6f91f8e0aa5616
Tiger Darbey <tiger@pop-os.localdomain>
2023-07-07 11:25:35 +0530

    restructure white space

--- src/components/wrappers/HeroWrapper/index.tsx
+++ src/components/wrappers/HeroWrapper/index.tsx
@@ -2,8 +2,9 @@ import { ReactNode } from 'react'
 import { heroWrapperStyles } from './styles'
 
 type HeroWrapperProps = {
-    children: ReactNode
+  children: ReactNode
 }
+
 export default function HeroWrapper(props: HeroWrapperProps) {
   const { classes } = heroWrapperStyles()
   return (
